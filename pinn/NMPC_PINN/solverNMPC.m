b635d5bad5a304dcbd58444dd2eca65d5bc02a8d
taniel <tanielfranklin@gmail.com>
2022-09-17 11:38:20 -0300

    fix

--- pinn/NMPC_PINN/solverNMPC.m
+++
@@ -1,7 +0,0 @@
-function [DuYsp,fval,flag,report] = solverNMPC(uk_1,Hp,Hc,q,r,qu,utg,Ts,nu,ny,ypk,xmk,ymk,ymk2,pm,DuYsp0,Ain,Bin,Aeq,beq,Dumin,Dumax)
-options = optimoptions(@fmincon,'display','off','algorithm','sqp','MaxIterations',30);
-
-[DuYsp,fval,flag,report] = fmincon(@(DuYsp)fob_NMPC(DuYsp,uk_1,Hp,Hc,q,r,qu,utg,Ts,nu,ny,ypk,xmk,ymk,ymk2,pm),DuYsp0,Ain,Bin,Aeq,beq,Dumin,Dumax,[],options);
-
-
-end
